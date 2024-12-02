0f7a05a91047c0393ecacb19083d8ee6cd798d7b
Shafei <alshafei.2430@gmail.com>
2024-12-02 17:59:48 +0200

    practcing nginx and docker

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const port = 3000
+const app = express()
+
+const replicaApp = process.env.APP_NAME
+
+app.use('/images', express.static(path.join(__dirname, 'images')))  
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+    console.log(`Req served by ${replicaApp}`)
+})
+
+app.listen(port, () => {
+    console.log(`${replicaApp} Server started at port ${port}`)
+})
